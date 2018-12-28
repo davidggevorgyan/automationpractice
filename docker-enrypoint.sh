ca8da0f1f2b51e9cee9165b703593cb6eea22047
davidggevorgyan <davidggevorgyan@gmail.com>
2018-12-28 14:22:22 +0400

    Now tests started with docker script will run automatically with zalenium images

--- docker-enrypoint.sh
+++ docker-enrypoint.sh
@@ -2,7 +2,7 @@
 docker-compose up --force-recreate | grep -q "Zalenium is now ready!"
 if [ $? == 0 ]; then
     # run initializing commands
-    mvn install test
+    mvn install test -Dselenium.remote=true
 fi
 
 mvn clean
