82dacd889aed2502724b66012bd4954e85878916
davidggevorgyan <davidggevorgyan@gmail.com>
2018-12-29 01:51:07 +0400

    Implemented multithreading with maven-surefire-plugin

--- docker-enrypoint.sh
+++ docker-enrypoint.sh
@@ -2,9 +2,7 @@
 docker-compose up --force-recreate | grep -q "Zalenium is now ready!"
 if [ $? == 0 ]; then
     # run initializing commands
-    mvn install test -Dselenium.remote=true
+    mvn clean test -Dselenium.remote=true
 fi
 
-mvn clean
 docker stop zalenium
-docker rm zalenium
