5aa29af3ced0a3dc7c7cf9bbbc6bf51404c232ef
davidggevorgyan <davidggevorgyan@gmail.com>
2019-01-04 06:32:34 +0400

    Fixing curl upload script

--- upload_script.sh
+++ upload_script.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -T ./target/report/report.html ftp://$ftpusername:$ftppassword@$ftppath/www/
+curl -T ./target/report/report.html --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 10 ftp://$ftpusername:$ftppassword@$ftppath/www/
