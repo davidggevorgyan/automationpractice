3177a54e0ef7af2ec3dccf09940a1e0867b9ca7c
davidggevorgyan <davidggevorgyan@gmail.com>
2019-01-06 23:41:31 +0400

    Wishlist test and page improvements

--- src/test/java/pages/WishlistPage.java
+++ src/test/java/pages/WishlistPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -11,6 +12,11 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class WishlistPage extends BasePage {
+	public void create(String wishlistName) {
+		type(newWishlistNameField, wishlistName);
+		click(submitNewWishlistButton);
+	}
+
 	@FindBy(className = "page-heading")
 	private WebElement myWhishlistHeader;
 
@@ -34,30 +40,49 @@ public class WishlistPage extends BasePage {
 		assertEquals(myWhishlistHeader.getText(), "MY WISHLISTS", "The page load is failed");
 	}
 
-	public void create(String name) {
-		type(newWishlistNameField, name);
-		click(submitNewWishlistButton);
+	public boolean isWishlistPresent(String wishlistName) {
+		isElementDisplayed(wishlistTable);
+		return getCell(wishlistName, Column.NAME) != null;
 	}
 
-	public boolean isWishilistPresent(String name) {
+	public void deletelWishlist(String wishlistName) {
 		isElementDisplayed(wishlistTable);
-		List<WebElement> allRows = wishlistTable.findElements(By.tagName("tr"));
+		WebElement wishlistDeleteButton = getCell(wishlistName, Column.DELETE);
+		wishlistDeleteButton.findElement(By.className("icon-remove")).click();
+		Alert popup = switchToAlert();
+		popup.accept();
+		isElementNotDisplayed(wishlistDeleteButton);
+	}
+
+	public WebElement getCell(String wishlistName, Column columnName) {
+		List<WebElement> allRows = wishlistTable.findElements(By.xpath("//*[contains(@class,\"table\")]/tbody/tr[*]/td[1]"));
+		int rowNumberCounter = 1;
 		for (WebElement row : allRows) {
-			List<WebElement> cells = row.findElements(By.tagName("td"));
-			if (cells.size() != 0 && cells.get(0).getText().equals(name)) {
-				return true;
+			if (row.getText().equals(wishlistName)) {
+				return wishlistTable.findElement(By.xpath("//*[contains(@class,\"table\")]/tbody/tr[" + rowNumberCounter + "]/td[" + columnName.columnNumber + "]"));
+			} else {
+				rowNumberCounter++;
 			}
 		}
-		return false;
+		throw new NoSuchElementException("Unable to locate {" + wishlistName + "} wishlist");
 	}
 
-	public void deleteAllWishlists() {
-		isElementDisplayed(wishlistTable);
-		List<WebElement> cells = wishlistTable.findElements(By.className("icon-remove"));
-		for (WebElement cell : cells) {
-			click(cell);
-			Alert popup = switchToAlert();
-			popup.accept();
+
+	enum Column {
+		NAME(1),
+		QTY(2),
+		VIEWED(3),
+		CREATED(4),
+		DIRECTLINK(5),
+		DELETE(6);
+
+		private final int columnNumber;
+
+		Column(int columnNumber) {
+			this.columnNumber = columnNumber;
 		}
 	}
+
+
 }
+
--- src/test/java/tests/WishlistTest.java
+++ src/test/java/tests/WishlistTest.java
@@ -11,20 +11,20 @@ import static org.testng.Assert.assertTrue;
 
 public class WishlistTest extends BaseTest {
 	@Test
-	public void createWishlist() {
+	public void createAddToDeleteWishlist() {
 		SignInPage signInPage = new SignInPage();
 		signInPage.signInWithCredentials("d1@grr.la", "d1@grr.la");
 
 		WishlistPage wishlistPage = new WishlistPage();
-		String whishlistName = String.valueOf(randomUUID()).substring(0, 8);
-		wishlistPage.create(whishlistName);
-		assertTrue(wishlistPage.isWishilistPresent(whishlistName), "Specified Whishilist was not found");
+		String wishlistName = String.valueOf(randomUUID()).substring(0, 8);
+		wishlistPage.create(wishlistName);
+		assertTrue(wishlistPage.isWishlistPresent(wishlistName), "Specified Wishlist was not found");
 
 		ItemPage itemPage = new ItemPage();
 		itemPage.clickAddToWishlist();
 		assertEquals(itemPage.getFancyBoxText(), "Added to your wishlist.", "FancyBox text was not valid");
 
 		wishlistPage.load();
-		wishlistPage.deleteAllWishlists();
+		wishlistPage.deletelWishlist(wishlistName);
 	}
 }
