3194bed36274dcf671aa4690f176150462da7644
davidggevorgyan <davidggevorgyan@gmail.com>
2019-01-08 23:11:40 +0400

    Fixed isSignOutLinkDisplayed test.

--- src/test/java/pages/SignInPage.java
+++ src/test/java/pages/SignInPage.java
@@ -64,7 +64,7 @@ public class SignInPage extends BasePage {
 	}
 
 	public boolean isSignOutLinkDisplayed() {
-		return isElementDisplayed(signInButton);
+		return isElementDisplayed(signOutLink);
 	}
 
 	public boolean isAlertBarDangerDisplayed() {
