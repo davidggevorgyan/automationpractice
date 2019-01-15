b87ec935cda235221134c94a6be0165e9d1bb10d
Travis CI <deploy@travis-ci.org>
2019-01-16 03:21:26 +0400

    allure deployment script fix

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,13 +3,14 @@
 mkdir target/github-pages
 cd target/github-pages
 git clone --single-branch --branch gh-pages https://davidggevorgyan:$GITHUB_TOKEN@github.com/davidggevorgyan/automationpractice.git .
-git config user.name "Travis CI"
-git config user.email "deploy@travis-ci.org"
 cp -R history ../allure-results/
 cd ../..
 mvn allure:report
 cp -R target/site/allure-maven-plugin/* target/github-pages/
+cp target/report/log4j-application.log target/github-pages/
 cd target/github-pages
-git add --all
+git config user.name "Travis CI"
+git config user.email "deploy@travis-ci.org"
+git add *
 git commit -m "Auto deploy from Travis CI build $TRAVIS_BUILD_NUMBER"
 git push -u origin gh-pages
