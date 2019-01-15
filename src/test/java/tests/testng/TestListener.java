a9dc7bfc0882cf9b80d73fb22c28cdc385060729
David Gevorgyan <davidgevorgyan@workfront.com>
2019-01-16 01:02:19 +0400

    allure support initial commit

--- src/test/java/tests/testng/TestListener.java
+++ src/test/java/tests/testng/TestListener.java
@@ -1,18 +1,13 @@
 package tests.testng;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 import org.apache.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import setup.DriverBase;
-import tests.utils.ExtentManager;
 
 public class TestListener implements ITestListener {
 	private final static Logger logger = Logger.getLogger(TestListener.class);
-	private static ExtentReports extent = ExtentManager.createInstance();
-	private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
 
 	@Override
 	public synchronized void onStart(ITestContext context) {
@@ -22,35 +17,29 @@ public class TestListener implements ITestListener {
 	@Override
 	public synchronized void onFinish(ITestContext context) {
 		logger.info(("Extent Reports Version 4  Test Suite is ending!"));
-		extent.flush();
 	}
 
 	@Override
 	public synchronized void onTestStart(ITestResult result) {
 		logger.info((result.getMethod().getMethodName() + " started!"));
-		ExtentTest extentTest = extent.createTest(result.getName()).assignCategory(result.getInstanceName());
-		test.set(extentTest);
 	}
 
 	@Override
 	public synchronized void onTestSuccess(ITestResult result) {
 		logger.info((result.getMethod().getMethodName() + " passed!"));
 		browserStackLinkGenerator();
-		test.get().pass("Test passed");
 	}
 
 	@Override
 	public synchronized void onTestFailure(ITestResult result) {
 		logger.info((result.getMethod().getMethodName() + " failed!"));
 		browserStackLinkGenerator();
-		test.get().fail(result.getThrowable());
 	}
 
 	@Override
 	public synchronized void onTestSkipped(ITestResult result) {
 		logger.info((result.getMethod().getMethodName() + " skipped!"));
 		browserStackLinkGenerator();
-		test.get().skip(result.getThrowable());
 	}
 
 	@Override
@@ -59,7 +48,7 @@ public class TestListener implements ITestListener {
 	}
 
 	private void browserStackLinkGenerator() {
-		test.get().info("<a href=\"https://automate.browserstack.com/builds/f535bea9b4c4f1050bccd80507ab9b175b959d8a/sessions/" + DriverBase.get().getSession(DriverBase.get().getDriver()) + "\" target=\"_blank\">Link to the Run</a>");
+		//test.get().info("<a href=\"https://automate.browserstack.com/builds/f535bea9b4c4f1050bccd80507ab9b175b959d8a/sessions/" + DriverBase.get().getSession(DriverBase.get().getDriver()) + "\" target=\"_blank\">Link to the Run</a>");
 	}
 
 }
