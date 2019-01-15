424c22856e15a4c3a9f4986fb6451feed9c6bb7f
David Gevorgyan <davidggevorgyan@gmail.com>
2019-01-16 02:47:05 +0400

    Allure deployment script implementation

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,11 @@ cd target/github-pages
 git clone --single-branch --branch gh-pages https://davidggevorgyan:$GITHUB_TOKEN@github.com/davidggevorgyan/automationpractice.git .
 git config user.name "Travis CI"
 git config user.email "deploy@travis-ci.org"
-cp  ../report/* ./
+cp -R history ../allure-results/
+cd ../..
+mvn allure:report
+cp -R target/site/allure-maven-plugin/* target/github-pages/
+cd target/github-pages
 git add --all
 git commit -m "Auto deploy from Travis CI build $TRAVIS_BUILD_NUMBER"
 git push -u origin gh-pages
