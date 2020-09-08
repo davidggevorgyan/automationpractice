d305c07831fbaa1f1ba7cd95c77c618a46dbba0f
David Gevorgyan <46099506+davidggevorgyan@users.noreply.github.com>
2020-09-08 15:36:31 +0400

    Update TestListener.java

--- src/test/java/tests/testng/TestListener.java
+++ src/test/java/tests/testng/TestListener.java
@@ -2,50 +2,48 @@ package tests.testng;
 
 import io.qameta.allure.*;
 import io.qameta.allure.model.Link;
-import org.apache.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import setup.*;
 
 public class TestListener implements ITestListener {
-	private final static Logger logger = Logger.getLogger(TestListener.class);
 
 	@Override
 	public synchronized void onStart(ITestContext context) {
-		logger.info("Test Suite started!");
+		System.out.println("Test Suite started!");
 	}
 
 	@Override
 	public synchronized void onFinish(ITestContext context) {
-		logger.info(("Test Suite is ending!"));
+		System.out.println(("Test Suite is ending!"));
 		FrameworkProperties.writeProperties();
 	}
 
 	@Override
 	public synchronized void onTestStart(ITestResult result) {
-		logger.info((result.getMethod().getMethodName() + " started!"));
+		System.out.println((result.getMethod().getMethodName() + " started!"));
 		browserStackLinkGenerator();
 	}
 
 	@Override
 	public synchronized void onTestSuccess(ITestResult result) {
-		logger.info((result.getMethod().getMethodName() + " passed!"));
+		System.out.println((result.getMethod().getMethodName() + " passed!"));
 	}
 
 	@Override
 	public synchronized void onTestFailure(ITestResult result) {
-		logger.info((result.getMethod().getMethodName() + " failed!"));
+		System.out.println((result.getMethod().getMethodName() + " failed!"));
 	}
 
 	@Override
 	public synchronized void onTestSkipped(ITestResult result) {
-		logger.info((result.getMethod().getMethodName() + " skipped!"));
+		System.out.println((result.getMethod().getMethodName() + " skipped!"));
 	}
 
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		logger.info(("onTestFailedButWithinSuccessPercentage for " + result.getMethod().getMethodName()));
+		System.out.println(("onTestFailedButWithinSuccessPercentage for " + result.getMethod().getMethodName()));
 	}
 
 	private void browserStackLinkGenerator() {
